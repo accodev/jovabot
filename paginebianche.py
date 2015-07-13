ecd27819f408066918dc33778cd2245424a10e38
OpenNingia <oppifjellet@gmail.com>
2015-07-13 16:36:14 +0200

    implemented paginebianche support

--- paginebianche.py
+++ paginebianche.py
@@ -29,29 +29,16 @@ def parse_response(text):
     address = ''
     phone = ''
 
-    with open('wp_dump.html', 'wt', encoding='utf-8') as fp:
-        fp.write(text)
-
-    m = NAME_PATTERN.match(text)
-    if m is not None and len(m.groups(1)) > 0:
-        name = m.groups(1) [0]
-
-    m = ADDRESS_PATTERN.match(text)
-    if m is not None and len(m.groups(1)) > 0:
-        address = m.groups(1) [0]
-
-    m = PHONE_PATTERN.match(text)
-    if m is not None and len(m.groups(1)) > 0:
-        phone = m.groups(1) [0]
-
+    #with open('wp_dump.html', 'wt', encoding='utf-8') as fp:
+    #    fp.write(text)
 
     soup = BeautifulSoup(text, 'html.parser')
 
     for d in soup.find_all("div", "vcard"):
         name = d.find('h2', 'rgs').a['title']
         phone = d.find("div", "tel").find("span", "value").text
-        address = d.find("div", "address").text
-        address = address[: address.index('|')].strip()
+        address = d.find("div", "address").div.text
+        #address = address[: address.index('|')].strip()
 
         yield wp_response(name, phone, address)
 
