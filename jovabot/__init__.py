84012a71f8ae34973693af53828786a7cd5db5d9
Marco Accorinti <accosk8@gmail.com>
2015-09-10 15:07:04 +0200

    migliorati un po' i percorsi di flask, aggiunta un po di sicurezza con il token telegram nell'url

--- jovabot/__init__.py
+++ jovabot/__init__.py
@@ -5,9 +5,10 @@ import datetime
 import importlib
 from . import modules
 import logging
+import socket
 
 import telegram
-from flask import Flask, request
+from flask import Flask, request, abort
 
 
 # ordered by priority
@@ -22,8 +23,12 @@ ENABLED_MODULES = [
 LOADED_MODULES = []
 
 bot = None
-
 webapp = Flask(__name__)
+
+# config section - change these as you like
+TOKEN = '1234567890abcdefgh'
+TOKEN_PATH = 'key.token'
+CERTIFICATE_PATH = '/etc/nginx/ssl/nginx.crt'
 
 logging.basicConfig(filename='jovabot.log', level=logging.DEBUG)
 
@@ -46,7 +51,7 @@ def jova_replace(s):
 
 def jova_do_something(message):
     if message.text:
-        if 'jova' in message.text.lower():  # invocato il dio supremo
+        if 'jova' in message.text.lower():  # jova, I choose you!
             logging.info(
                 "[{0}] [from {1}] [message ['{2}']]".format(datetime.datetime.now().isoformat(), message.from_user,
                                                             message.text))
@@ -91,63 +96,75 @@ def init_modules():
         m.init()
 
 
-@webapp.route('/telegram', methods=['POST'])
-def telegram_hook():
-    logging.info("telegram message arrived")
-    # retrieve the message in JSON and then transform it to Telegram object
-    req = request.get_json(force=True)
-    logging.info(req)
-    update = telegram.Update.de_json(req)
+@webapp.route('/telegram/<token>', methods=['POST'])
+def telegram_hook(token):
+    if token == TOKEN:
+        # retrieve the message in JSON and then transform it to Telegram object
+        update = telegram.Update.de_json(request.get_json(force=True))
 
+        # do something, man!
+        jova_do_something(update.message)
 
-    # log some shitz
-    logging.info(update.__dict__)
-    logging.info(update.message.__dict__)
-    logging.info(update.message.chat.__dict__)
-
-    # do something, man!
-    jova_do_something(update.message)
-
-    # jova return something ffs!
-    return "ok", 200
+        # jova return something ffs!
+        return "ok", 200
+    else:
+        return "ko", 400
 
 
 @webapp.route('/')
 def hello():
-    return "hello!"
+    return "jovabot was here!"
 
 
-@webapp.route('/webhook/set')
-def webhook_set():
-    with open('/etc/nginx/ssl/nginx.crt') as c:
-        res = bot.setWebhook(webhook_url='https://acco.duckdns.org/jovabot/telegram', certificate=c.buffer)
-        logging.info(res)
+@webapp.route('/webhook/<command>')
+def webhook(command):
+    if command == 'set':
+        res = webhook_set()
+    elif command == 'delete':
+        res = webhook_delete()
+    else:
+        res = 'unsupported command {0}'.format(command)
+        abort(400)
 
-    return 'ok', 200
-
-
-@webapp.route('/webhook/delete')
-def webhook_delete():
-    res = bot.setWebhook('')
     logging.info(res)
 
-    return 'ok', 200
+    return res, 200
+
+
+def webhook_set():
+    # use your nginx.crt man!
+    with open(CERTIFICATE_PATH) as c:
+        webhook_url = socket.gethostname() + '/jovabot/telegram/' + TOKEN
+        res = bot.setWebhook(webhook_url=webhook_url, certificate=c.buffer)
+    return res
+
+
+def webhook_delete():
+    res = bot.setWebhook('')
+    return res
 
 
 @webapp.before_first_request
 def main():
     logging.info("starting up")
+
+    # processid file - useful to track jovabot
     pid = str(os.getpid())
     pidfile = "jovabot.pid"
 
     with open(pidfile, "w") as p:
         p.write(pid)
 
+    # load jovabot modules - crazy stuff
     load_modules()
     init_modules()
 
+    # telegram bot api token
+    global TOKEN
+    TOKEN = extract_token(TOKEN_PATH)
+
     global bot
-    bot = telegram.Bot(token=extract_token("key.token"))
+    bot = telegram.Bot(token=TOKEN)
 
 
 if __name__ == '__main__':
