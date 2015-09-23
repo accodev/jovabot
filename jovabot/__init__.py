bc6c2c1b0584a5ef54826fb6bded77d378b83662
Marco Accorinti <accosk8@gmail.com>
2015-09-23 15:20:04 +0200

    piccionate

--- jovabot/__init__.py
+++ jovabot/__init__.py
@@ -63,7 +63,8 @@ def jova_do_something(message):
                 if isinstance(answer, tuple):
                     if answer[1]:
                         answer = answer[0]
-                        md = True if answer[1] == 'markdown' else md = False
+                        if answer[1] == 'markdown':
+                            md = True
                 else:
                     answer = jova_replace(answer)
                 bot.sendChatAction(chat_id=chat_id, action=telegram.ChatAction.TYPING)
