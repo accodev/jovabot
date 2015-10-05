62d1d3288d76bfaa225a8c573a3afb4d69b0e694
Marco Accorinti <accosk8@gmail.com>
2015-10-05 11:51:59 +0200

    Stianta

--- jovabot/modules/addressbook/paginebianche.py
+++ jovabot/modules/addressbook/paginebianche.py
@@ -5,8 +5,9 @@ import logging
 
 from bs4 import BeautifulSoup
 
-#White pages url
-WHITE_PAGES_URL="http://www.paginebianche.it/execute.cgi"
+# White pages url
+WHITE_PAGES_URL = "http://www.paginebianche.it/execute.cgi"
+
 
 class wp_response(object):
     def __init__(self, name, tel, addr):
@@ -14,23 +15,25 @@ class wp_response(object):
         self.tel = tel
         self.addr = addr
 
+
 def search_wp(name, location):
     payload = {'btt': '1', 'qs': name, 'dv': location}
 
     try:
         r = requests.get(WHITE_PAGES_URL, params=payload)
-        logging.debug('requesting', r.url)
+        logging.debug('requesting {0}', r.url)
         return parse_response(r.text)
     except Exception as e:
         logging.debug('search failed', e)
         return None
+
 
 def parse_response(text):
     name = ''
     address = ''
     phone = ''
 
-    #with open('wp_dump.html', 'wt', encoding='utf-8') as fp:
+    # with open('wp_dump.html', 'wt', encoding='utf-8') as fp:
     #    fp.write(text)
 
     soup = BeautifulSoup(text, 'html.parser')
@@ -39,9 +42,10 @@ def parse_response(text):
         name = d.find('h2', 'rgs').a['title']
         phone = d.find("div", "tel").find("span", "value").text
         address = d.find("div", "address").div.text
-        #address = address[: address.index('|')].strip()
+        # address = address[: address.index('|')].strip()
 
         yield wp_response(name, phone, address)
+
 
 def test():
     name = sys.argv[1]
@@ -49,6 +53,7 @@ def test():
 
     for o in search_wp(name, loc):
         logging.debug(o.__dict__)
+
 
 if __name__ == "__main__":
     test()
